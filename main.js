a4f54309b565a9b6505b8c63c361b089d6d4df8f
JairKevinFG <jkfloresg@uni.pe>
2020-10-06 22:09:31 -0500

    buscar pokemon

--- main.js
+++ main.js
@@ -2,17 +2,14 @@ const container = document.querySelector('.container')
 const result = document.querySelector('#result')
 const form = document.querySelector('#form')
 
-
-form.addEventListener('submit',valid)
-
-
-const valid = (event) => {
+form.addEventListener('submit', (event)=> {
     event.preventDefault()
     const pokemon = document.querySelector('#pokemon').value
     if(pokemon === ''){
         showError("Campo vacio")
     }
-}
+    searchPokemon(pokemon)
+})
 
 const showError = (message) => {
     const confirm = document.querySelector('.message')
@@ -26,5 +23,26 @@ const showError = (message) => {
         },3000)
     }
 }
+
+const searchPokemon= (name) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${name}`
+    console.log(url)
+    fetch(url).then(datos => {
+        return datos.json()
+    }).then(datos => {
+        showPokemon(datos)
+    })
+}
+
+const showPokemon = (datos) => {
+    const {name, sprites :{other:{dream_world:{ front_default}}} } = datos
+    result.innerHTML = `<img src="${front_default}"> `
+    console.log(name)
+    console.log(front_default)
+}
+
+
+
+
 
 
